dc74b0d0d7608fab2207e17f558a25ab56f498bc
Ryan MacEachern <ryan.maceachern@gmail.com>
2022-05-05 20:56:18 -0400

    push title and href vars to articles arr

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 
-const app = express()
-
+const app = express();
 const PORT = 3000;
+const dest = 'https://www.usedvictoria.com/all?q=dumbbell&r=greatervictoria';
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}.`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
+
+axios(dest)
+  .then(res => {
+    const html = res.data;
+    console.log(html);
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $('.title', html).each(function() {
+      const title = $(this).text()
+      const url = $(this).find('a').attr('href')
+      articles.push({
+        title,
+        url
+      });
+    });
+    console.log(articles);
+  })
+  .catch(e => {
+    console.log("Error:", e);
+  });
